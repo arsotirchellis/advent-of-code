8876831985fe36a79d6a881464867d494cdb2795
arsotirchellis <arsotirchellis@gmail.com>
2024-12-01 21:02:19 +0200

    day 1

--- 2024/Day1/part1.py
+++ 2024/Day1/part1.py
@@ -5,4 +5,19 @@ with open(path, "r") as file:
     contents = file.read()
 lines = [x for x in contents.split("\n")]
 
-print("Good luck !")
+left_list = []
+right_list = []
+for line in lines:
+    left, right = line.split()
+    left_list.append(int(left))
+    right_list.append(int(right))
+
+
+left_list = sorted(left_list)
+right_list = sorted(right_list)
+
+dist_sum = 0
+for i in range(len(left_list)):
+    dist_sum += abs(left_list[i] - right_list[i])
+
+print(dist_sum)
--- 2024/Day1/part2.py
+++ 2024/Day1/part2.py
@@ -5,4 +5,28 @@ with open(path, "r") as file:
     contents = file.read()
 lines = [x for x in contents.split("\n")]
 
-print("Good luck !")
+left_list = []
+right_list = []
+for line in lines:
+    left, right = line.split()
+    left_list.append(int(left))
+    right_list.append(int(right))
+
+
+left_list = sorted(left_list)
+right_list = sorted(right_list)
+
+# dict: key=number value:number of occurences
+right_list_occurences_dict = {}
+for number in right_list:
+    if number not in right_list_occurences_dict:
+        right_list_occurences_dict[number] = 1
+    else:
+        right_list_occurences_dict[number] += 1
+
+dist_sum = 0
+for number in left_list:
+    if number in right_list_occurences_dict:
+        dist_sum += number*right_list_occurences_dict[number]
+
+print(dist_sum)
