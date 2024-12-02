9ba5ac95c13fb4a46595105808ace299f1ae49db
arsotirchellis <arsotirchellis@gmail.com>
2024-12-02 19:32:49 +0200

    day-2

--- 2024/Day2/part1.py
+++ 2024/Day2/part1.py
@@ -1,8 +1,37 @@
 from pathlib import Path
 
-path = Path(__file__).parent / "input.txt"
+path = Path(__file__).parent / "test.txt"
 with open(path, "r") as file:
     contents = file.read()
 lines = [x for x in contents.split("\n")]
 
-print("Good luck !")
+reports = [[int(num) for num in x.split(" ") if num] for x in lines if x]
+
+def is_safe(report):
+    min_level_increase = 1
+    max_level_increase = 3
+
+    increasing = True
+    decreasing = True
+
+    for i in range(1, len(report)):
+        if increasing:
+            if (report[i] - report[i - 1] < min_level_increase) or (report[i] - report[i - 1] > max_level_increase):
+                increasing = False
+        if decreasing:
+            if (report[i] - report[i - 1] < max_level_increase * -1) or (report[i] - report[i - 1] > min_level_increase * -1):
+                decreasing = False
+
+        if not increasing and not decreasing:
+            break
+
+    return increasing or decreasing
+
+
+
+valid_reports = 0
+for report in reports:
+    if is_safe(report):
+        valid_reports += 1
+
+print(valid_reports)
--- 2024/Day2/part2.py
+++ 2024/Day2/part2.py
@@ -5,4 +5,49 @@ with open(path, "r") as file:
     contents = file.read()
 lines = [x for x in contents.split("\n")]
 
-print("Good luck !")
+reports = [[int(num) for num in x.split(" ") if num] for x in lines if x]
+
+def is_safe(report, recursive=False):
+    min_level_increase = 1
+    max_level_increase = 3
+
+    increasing = True
+    decreasing = True
+
+    increasing_problematic_index = -1
+    decreasing_problematic_index = -1
+
+    for i in range(1, len(report)):
+        if increasing:
+            if (report[i] - report[i - 1] < min_level_increase) or (report[i] - report[i - 1] > max_level_increase):
+                increasing = False
+                increasing_problematic_index = i
+        if decreasing:
+            if (report[i] - report[i - 1] < max_level_increase * -1) or (report[i] - report[i - 1] > min_level_increase * -1):
+                decreasing = False
+                decreasing_problematic_index = i
+        if not increasing and not decreasing:
+            break
+
+    if not increasing and not decreasing:
+        if recursive:
+            report_without_increasing_problematic_element = report[:increasing_problematic_index] + report[increasing_problematic_index + 1:]
+            report_without_decreasing_problematic_element = report[:decreasing_problematic_index] + report[decreasing_problematic_index + 1:]
+            report_without_first_element = report[1:]
+            report_without_last_element = report[:-1]
+            second_chance = is_safe(report_without_increasing_problematic_element) or is_safe(report_without_decreasing_problematic_element) or is_safe(report_without_first_element) or is_safe(report_without_last_element)
+            return second_chance
+        else:
+            return False
+    else:
+        return True
+
+
+
+valid_reports = 0
+for report in reports:
+    if is_safe(report, recursive=True):
+
+        valid_reports += 1
+
+print(valid_reports)
